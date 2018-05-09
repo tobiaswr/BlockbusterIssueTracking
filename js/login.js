484a6a45246fb89a9982d5ccf4b5a3f7e29efef5
Tobias Walmann Rognstad <tobiaswrognstad@gmail.com>
2018-05-09 12:20:56 +0200

    Created login site and functionality.

--- js/login.js
+++ js/login.js
@@ -7,8 +7,18 @@ $(document).ready(() => {
         $.get("http://localhost:3000/login", function(result){
             for(var i = 0; i<result.length; i++){
                 if(username == result[i].username && password == result[i].password){
-                    window.location = "index.html";
-                    return;
+                    if (result[i].stilling == "Translator"){
+                        window.location = "translator.html";
+                        return;
+                    }
+                    else if(result[i].stilling == "Product Manager"){
+                        window.location = "index.html";
+                        return;
+                    }
+                    else{
+                        window.location = "contentmanager.html";
+                        return;
+                    }
                 }
             }
             alert("Du har enten tastet noe feil, eller så eksisterer ikke brukeren.");
